3b6b2bbc3d2be96860516eda064b79deefc8cfc3
Gus Deribeaux <Gusderibeaux@gmail.com>
2023-02-02 17:22:14 -0500

    Create indivual dummy poster

--- src/Individual/Individual.js
+++ src/Individual/Individual.js
@@ -2,21 +2,22 @@ import React from "react";
 import "./Individual.css";
 
 
-const Individual = ({title, poster_path, backdrop_path, release_date, overview, average_rating, genres, budget, revenew, runtime, tagline}) => {
-    // console.log('titlee', individualInfo)
+const Individual = ({individualInfo, selectPoster, id}) => {
+    // console.log('titlee', individualInfo.individualInfo.title)
     return (
         <div>Hello
-            <p>{title}</p>
-            <img src={poster_path}></img>
-            <img src={backdrop_path}></img>
-            <p>{release_date}</p>
-            <p>{overview}</p>
-            <p>{average_rating}</p>
-            <p>{genres}</p>
-            <p>{budget}</p>
-            <p>{revenew}</p>
-            <p>{runtime}</p>
-            <p>{tagline}</p>
+            <p>{individualInfo.title}</p>
+            <img src={individualInfo.poster_path}></img>
+            <img src={individualInfo.backdrop_path}></img>
+            <p>{individualInfo.release_date}</p>
+            <p>{individualInfo.overview}</p>
+            <p>{individualInfo.average_rating}</p>
+            <p>{individualInfo.genres}</p>
+            <p>{individualInfo.budget}</p>
+            <p>{individualInfo.revenew}</p>
+            <p>{individualInfo.runtime}</p>
+            <p>{individualInfo.tagline}</p>
+            <button onClick = {() => selectPoster(id)}>GO BACK HOME</button>
         </div>
     )
 }
